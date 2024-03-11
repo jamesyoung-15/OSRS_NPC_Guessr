cceb00f8adc134674e841d6edd66dee4b6e22bbb
jamesyoung <jamesyoung3931@gmail.com>
2024-03-11 15:11:45 +0800

    add map marker

--- client/js/map.js
+++ client/js/map.js
@@ -1,13 +1,33 @@
 // OSRS World Map link (8793x4993)
 let osrsMapLink = 'https://cdn.runescape.com/assets/img/external/oldschool/2023/newsposts/2023-07-28/osrs_world_map_july13_2023.png';
 
-var map = L.map('map', {
+// create leaflet coordinate reference system map
+let map = L.map('map', {
     crs: L.CRS.Simple, // Set the coordinate reference system
     minZoom: 1,
     maxZoom: 20
 });
+// create approximate boundaries
 let imageBounds = [[0,0], [1000,1000]];
-L.imageOverlay(osrsMapLink, imageBounds).addTo(map);
 
+// create marker
+let marker = L.marker([445, 750])
+  .addTo(map);
+
+// create marker on map click
+let onMapClick = (e) => {
+    let latitude = e.latlng.lat; // Get the latitude of the clicked point
+    let longitude = e.latlng.lng; // Get the longitude of the clicked point
+
+    // Create a marker at the clicked location
+    marker.setLatLng([latitude, longitude]);
+    console.log("You clicked the map at " + e.latlng.toString())
+}
+
+
+
+
+L.imageOverlay(osrsMapLink, imageBounds).addTo(map);
 map.fitBounds(imageBounds);
 map.setView([445, 750], 3);
+map.on('click', onMapClick);
